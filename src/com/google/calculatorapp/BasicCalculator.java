584847115f4fa22fe3008173f1969cefa5ba8442
Gireesh <grsh0674@gmail.com>
2024-07-26 16:42:12 -0500

    Second commit

--- src/com/google/calculatorapp/BasicCalculator.java
+++ src/com/google/calculatorapp/BasicCalculator.java
@@ -3,7 +3,7 @@ package com.google.calculatorapp;
 
 public class BasicCalculator {
 
-public int result,n1,n2,n3,n4,n5;
+public int result,n1,n2,n3,n4,n5; /**Decaring result and n values as int   */
 	
 	public void Calculation() {
 		System.out.println("==This is a Arthemetic Calculation==");
